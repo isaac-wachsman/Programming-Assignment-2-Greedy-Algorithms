955c4daf10db6b1abe88795793dbe0acf24c6167
aaravsgit <aarav.bejjinki@gmail.com>
2026-03-06 19:48:14 -0500

    Added driver.h file

---
+++ src/driver.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int fifo(int capacity, vector<int> requests);
+
+int lru(int capacity, vector<int> requests);
+
+int optff(int capacity, vector<int> requests);
